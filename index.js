76d846be2fb6647f599c75df5722906ddf584b2b
Michael Stafford <michaelstafford808@gmail.com>
2021-12-02 14:00:26 -0800

    commit tree afa18775dc608320d6e00926fe9d12a2e0922bd3

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+    const returnFirstTwoDrivers = function (arrayOfDrivers) {
+        
+        return arrayOfDrivers.slice(0,2);
+    };
+
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+    
+    return arrayOfDrivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(multiply) {
+    return function(value) {
+        return value * multiply
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+const selectDifferentDrivers = (selectingDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(selectingDrivers, returnFirstTwoDrivers);
+}
+
+
+// const selectDifferentDrivers = (selectingDrivers) => {
+//     return selectingDrivers(returnFirstTwoDrivers, returnLastTwoDrivers);
+// }
+
+
+// console.log(selectingDifferentDrivers);
+
